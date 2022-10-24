fbf13528aa29125af44573845b4366dcfdbcaf48
d.pishnenko <d.pishnenko@tg.dunice.net>
2022-10-24 19:00:23 +0300

    Add routing to submissions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SubmissionsComponent } from './pages/submissions/submissions.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'submissions', component: SubmissionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
