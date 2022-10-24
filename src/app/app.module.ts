d7066d873386777d504fae0cc84369b999c9d865
d.pishnenko <d.pishnenko@tg.dunice.net>
2022-10-24 19:04:19 +0300

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SubmissionsComponent } from './pages/submissions/submissions.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubmissionsComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
