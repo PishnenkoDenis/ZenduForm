997e4ed3fb7f7ce147c7477eef94dc83f6bf6ab3
d.pishnenko <d.pishnenko@tg.dunice.net>
2022-10-26 18:27:25 +0300

    Add styles, images and packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from  '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SubmissionsComponent } from './pages/submissions/submissions.component';
 import { HeaderComponent } from './components/header/header.component';
+import { JwPaginationModule } from 'jw-angular-pagination';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SubmissionsComponent,
     HeaderComponent,
+    PaginationComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    JwPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
